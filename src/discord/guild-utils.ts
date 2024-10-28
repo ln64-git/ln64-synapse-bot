686b2170a483b79548df65312f2477705148110d
Luke Anthony <ln64.ohio@gmail.com>
2024-10-28 02:02:45 -0400

    removed unused functions

--- src/discord/guild-utils.ts
+++ src/discord/guild-utils.ts
@@ -4,16 +4,12 @@ import {
   ChatInputCommandInteraction,
   Collection,
   Guild,
-  type GuildBasedChannel,
   GuildMember,
   Message,
   PermissionsBitField,
   Snowflake,
   TextChannel,
 } from "npm:discord.js";
-import pLimit from "npm:p-limit";
-import process from "node:process";
-import { ChannelType } from "npm:discord-api-types/v10";
 import type { Db } from "npm:mongodb@5.6.0";
 import OpenAI from "npm:openai";
 import type { DiscordMessageWithEmbedding } from "../types.ts";
@@ -40,7 +36,6 @@ export function validateInteraction(
   return { guild, user, days };
 }
 
-// Utility function to check channel permissions
 export function checkChannelPermissions(
   textChannel: TextChannel,
   guild: Guild, // guild is used to fetch user channels
@@ -50,214 +45,6 @@ export function checkChannelPermissions(
     permissions?.has(PermissionsBitField.Flags.ViewChannel) &&
       permissions.has(PermissionsBitField.Flags.ReadMessageHistory),
   );
-}
-
-// Utility function to fetch all messages from a specific channel
-export async function fetchMessagesFromGuildChannel(
-  channel: Channel,
-  count?: number,
-): Promise<DiscordMessageWithEmbedding[]> {
-  if (!channel || !(channel instanceof TextChannel)) {
-    throw new Error("Invalid channel or not a text channel");
-  }
-
-  let allMessages: DiscordMessageWithEmbedding[] = [];
-  let lastMessageId: string | undefined;
-
-  while (true) {
-    console.log(
-      `Fetching messages from channel ${channel.name} before message ID ${lastMessageId}`,
-    );
-    // Fetch messages in batches of 100
-    const messages: Collection<string, DiscordMessageWithEmbedding> =
-      await channel.messages.fetch({
-        limit: 100,
-        before: lastMessageId,
-      });
-
-    allMessages = allMessages.concat(Array.from(messages.values()));
-
-    console.log(
-      `Fetched ${messages.size} messages from channel ${channel.name}`,
-    );
-    console.log(
-      `Messages processed: ${allMessages.length}, Messages left to process: ${messages.size}`,
-    );
-
-    if (messages.size < 100 || (count && allMessages.length >= count)) {
-      // No more messages to fetch or reached the message count limit
-      console.log(`No more messages to fetch from channel ${channel.name}`);
-      break;
-    }
-
-    // Set the last message ID to fetch the next batch
-    lastMessageId = messages.last()?.id;
-  }
-
-  // If messageCount is specified, slice the array to the required length
-  if (count) {
-    allMessages = allMessages.slice(0, count);
-  }
-
-  // Reverse the order of messages to be from oldest to newest
-  allMessages.reverse();
-
-  console.log(
-    `Total messages fetched from channel ${channel.name}: ${allMessages.length}`,
-  );
-  return allMessages;
-}
-
-// Utility function to fetch a single member from a guild by user ID
-export async function fetchMemberFromGuild(
-  guild: Guild,
-  userId: Snowflake,
-): Promise<GuildMember | null> {
-  const start = performance.now();
-  try {
-    const member = await guild.members.fetch(userId);
-    const end = performance.now();
-    console.info(`fetchMemberFromGuild took ${end - start} ms`);
-    return member;
-  } catch (error) {
-    console.error(`Failed to fetch member with ID ${userId}:`, error);
-    return null;
-  }
-}
-
-// Utility function to fetch all mentions of a specific member from a guild
-export async function fetchMemberMentionsFromGuild(
-  guild: Guild,
-): Promise<DiscordMessageWithEmbedding[]> {
-  const start = process.hrtime.bigint();
-  const limit = pLimit(5);
-  const textChannels = guild.channels.cache.filter(
-    (channel: GuildBasedChannel) => channel.type === ChannelType.GuildText,
-  ) as Map<Snowflake, TextChannel>;
-
-  const allMessages: DiscordMessageWithEmbedding[] = [];
-
-  await Promise.all(
-    Array.from(textChannels.values()).map((textChannel: TextChannel) =>
-      limit(async () => {
-        console.info(
-          `fetching messages from channel ${textChannel.name}... `,
-        );
-
-        try {
-          if (await checkChannelPermissions(textChannel, guild)) {
-            const messages = await fetchMessagesFromGuildChannel(
-              textChannel,
-            );
-            if (messages.length > 0) {
-              console.info(`Mentions found in channel: ${textChannel.name}`);
-            }
-            allMessages.push(...messages);
-          }
-        } catch (error) {
-          console.error(
-            `Error fetching messages from channel ${textChannel.name}: ${error}`,
-          );
-        }
-      })
-    ),
-  );
-
-  const end = BigInt(Date.now()) * BigInt(1e6);
-  console.info(
-    `fetchMemberMentionsFromGuild took ${(end - start) / BigInt(1e6)} ms`,
-  );
-  return allMessages;
-}
-
-// Utility function to fetch all messages from a guild
-export async function fetchAllMessagesFromGuild(
-  guild: Guild,
-): Promise<Message[]> {
-  const start = performance.now();
-  const limit = pLimit(5); // Limit concurrent requests to avoid rate limits
-  const textChannels = guild.channels.cache.filter(
-    (channel: GuildBasedChannel) => channel.type === ChannelType.GuildText,
-  ) as Map<Snowflake, TextChannel>;
-
-  const allMessages: DiscordMessageWithEmbedding[] = [];
-
-  await Promise.allSettled(
-    Array.from(textChannels.values()).map((textChannel: TextChannel) =>
-      limit(async () => {
-        try {
-          if (await checkChannelPermissions(textChannel, guild)) {
-            const messages = await fetchMessagesFromGuildChannel(
-              textChannel,
-            );
-            allMessages.push(...messages);
-          }
-        } catch (error) {
-          console.error(
-            `Error fetching messages from channel ${textChannel.name}: ${error}`,
-          );
-        }
-      })
-    ),
-  );
-
-  const end = performance.now();
-  console.info(`fetchAllMessagesFromGuild took ${end - start} ms`);
-  return allMessages;
-}
-
-// Utility function to get a channel from its ID
-export function getChannelFromId(
-  guild: Guild,
-  channelId: Snowflake,
-): GuildBasedChannel | null {
-  const channel = guild.channels.cache.get(channelId);
-  if (!channel) {
-    console.log(
-      `Channel with ID ${channelId} not found in guild ${guild.name}`,
-    );
-    return null;
-  }
-  return channel;
-}
-
-export async function fetchAllMembersFromGuild(
-  guild: Guild,
-): Promise<GuildMember[]> {
-  const members: GuildMember[] = [];
-  let lastMemberId: Snowflake | undefined;
-  const limit = 1000; // Discord's maximum limit for fetching members
-
-  while (true) {
-    console.log(`Fetching members after ID: ${lastMemberId || "start"}`);
-
-    // Fetch members in batches with an optional starting point
-    const fetchedMembers = await guild.members.fetch({
-      limit,
-    }) as Collection<Snowflake, GuildMember>;
-
-    // If no members are fetched, break the loop
-    if (fetchedMembers.size === 0) {
-      console.log("No more members to fetch.");
-      break;
-    }
-
-    // Add fetched members to the list
-    members.push(...fetchedMembers.values());
-
-    // Update `lastMemberId` for the next batch
-    lastMemberId = fetchedMembers.last()?.id;
-
-    // If fetched members are fewer than the limit, we're done
-    if (fetchedMembers.size < limit) {
-      console.log("Fetched all members.");
-      break;
-    }
-  }
-
-  console.info(`fetchAllMembersFromGuild took ${performance.now()} ms`);
-  console.log(`Total members fetched: ${members.length}`);
-  return members;
 }
 
 export async function syncMembersToDatabase(
